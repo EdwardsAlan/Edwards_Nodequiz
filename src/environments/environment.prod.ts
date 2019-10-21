d25d8850b9227afa223d4a8ab456e4587bbec042
EdwardsAlan <alan.edwards1103@gmail.com>
2019-10-20 23:09:32 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
+/*
+============================================
+; Title: NodeQuiz
+; Author: Alan Edwards
+; Date: Oct 6 2019
+; Description: NodeQuiz
+;===========================================
+*/
 export const environment = {
   production: true
 };
