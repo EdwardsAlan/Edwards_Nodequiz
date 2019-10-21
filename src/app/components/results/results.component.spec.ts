48b5728737b5f0b4023c6914f6b9e19b83e8f80b
EdwardsAlan <alan.edwards1103@gmail.com>
2019-10-20 23:07:47 -0400

    Update results.component.spec.ts

--- src/app/components/results/results.component.spec.ts
+++ src/app/components/results/results.component.spec.ts
@@ -1,3 +1,11 @@
+/*
+============================================
+; Title: NodeQuiz
+; Author: Alan Edwards
+; Date: Oct 6 2019
+; Description: NodeQuiz
+;===========================================
+*/
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ResultsComponent } from './results.component';
